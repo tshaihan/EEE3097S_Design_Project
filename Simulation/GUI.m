3268f19b3ed480af55a05fdef5ca9450696f6826
aimeesimons <smnaim002@myuct.ac.za>
2023-09-11 14:24:32 +0200

    Update GUI

--- Simulation/GUI.m
+++ Simulation/GUI.m
@@ -1,4 +1,4 @@
-classdef GUI < matlab.apps.AppBase
+classdef app1 < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
@@ -12,70 +12,96 @@ classdef GUI < matlab.apps.AppBase
         XCoordinateEditField       matlab.ui.control.NumericEditField
         XCoordinateEditFieldLabel  matlab.ui.control.Label
         FindSoundSourceButton      matlab.ui.control.Button
-        InitialiseMicsButton       matlab.ui.control.Button
         UIAxes                     matlab.ui.control.UIAxes
     end
+
     
     properties (Access = private)
         m1_x = 0.0
         m1_y = 0.0
-        
+
         m2_x = 0.0
         m2_y = 0.5
-        
+
+
         m3_x = 0.8
         m3_y = 0.0
-        
+
+
         m4_x = 0.8
         m4_y = 0.5
     end
     
-    % Callbacks that handle component events
     methods (Access = private)
         
-        % Button pushed function: InitialiseMicsButton
-        function InitialiseMicsButtonPushed(app, event)
-            plot(app.UIAxes,app.m1_x*1.25,app.m1_y*2, 'o')
-            hold(app.UIAxes, 'on');
+        function Initialise_Mics(app)
+        plot(app.UIAxes,app.m1_x*1.25,app.m1_y*2, 'o')
+        hold(app.UIAxes, 'on');
+
+        plot(app.UIAxes,app.m2_x*1.25,app.m2_y*2, 'o')
+        hold(app.UIAxes, 'on');
+
+        plot(app.UIAxes,app.m3_x*1.25,app.m3_y*2, 'o')
+        hold(app.UIAxes, 'on');
+
+        plot(app.UIAxes,app.m4_x*1.25,app.m4_y*2, 'o')
+        hold(app.UIAxes, 'on');
             
-            plot(app.UIAxes,app.m2_x*1.25,app.m2_y*2, 'o')
-            hold(app.UIAxes, 'on');
-            
-            plot(app.UIAxes,app.m3_x*1.25,app.m3_y*2, 'o')
-            hold(app.UIAxes, 'on');
-            
-            plot(app.UIAxes,app.m4_x*1.25,app.m4_y*2, 'o')
-            hold(app.UIAxes, 'on');
         end
-        
+    end
+    
+
+    % Callbacks that handle component events
+    methods (Access = private)
+
+        % Code that executes after component creation
+        function startupFcn(app)
+            Initialise_Mics(app); 
+            plot(app.UIAxes,0.5,0.5,"*")
+            hold(app.UIAxes,"on")
+            text(app.UIAxes,0.5,0.5,'Calibration Signal')
+            hold(app.UIAxes,"on")
+        end
+
         % Button pushed function: FindSoundSourceButton
         function FindSoundSourceButtonPushed(app, event)
             x_s = app.XCoordinateEditField.Value;
             y_s = app.YCoordinateEditField.Value;
+
+            if (x_s>0.8||x_s<0)||(y_s>0.5||y_s<0)
+                f = msgbox("Position out of bounds, please stay within the grid","Error","error");
+            else 
+                plot(app.UIAxes, x_s*1.25, y_s*2,'*')
+                hold(app.UIAxes, 'on');
+    
+                coords = Simulation_v3(x_s,y_s);
+    
+                plot(app.UIAxes,coords(1)*1.25,coords(2)*2,"-s")
+                hold(app.UIAxes,'on')
+    
+                app.Label.Text = string(coords(1));
+                app.Label_2.Text = string(coords(2));
+            end
+
+
+
             
-            plot(app.UIAxes, x_s*1.25, y_s*2,'*')
-            hold(app.UIAxes, 'on');
-            
-            coords = main(x_s,y_s);
-            
-            plot(app.UIAxes,coords(1)*1.25,coords(2)*2,"-s")
-            hold(app.UIAxes,'on')
-            
-            app.Label.Text = string(coords(1))
-            app.Label_2.Text = string(coords(2))
+
+
         end
     end
-    
+
     % Component initialization
     methods (Access = private)
-    
+
         % Create UIFigure and components
         function createComponents(app)
+
             % Create UIFigure and hide until all components are created
             app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 640 374];
+            app.UIFigure.Position = [100 100 640 317];
             app.UIFigure.Name = 'MATLAB App';
-    
+
             % Create UIAxes
             app.UIAxes = uiaxes(app.UIFigure);
             title(app.UIAxes, 'A1 Grid')
@@ -90,85 +116,82 @@ classdef GUI < matlab.apps.AppBase
             app.UIAxes.YTickLabel = {'0'; '0.1'; '0.2'; '0.3'; '0.4'; '0.5'};
             app.UIAxes.XGrid = 'on';
             app.UIAxes.YGrid = 'on';
-            app.UIAxes.Position = [212 67 408 282];
-    
-            % Create InitialiseMicsButton
-            app.InitialiseMicsButton = uibutton(app.UIFigure, 'push');
-            app.InitialiseMicsButton.ButtonPushedFcn = createCallbackFcn(app, @InitialiseMicsButtonPushed, true);
-            app.InitialiseMicsButton.Position = [382 20 100 23];
-            app.InitialiseMicsButton.Text = 'Initialise Mics';
-    
+            app.UIAxes.Position = [212 10 408 282];
+
             % Create FindSoundSourceButton
             app.FindSoundSourceButton = uibutton(app.UIFigure, 'push');
             app.FindSoundSourceButton.ButtonPushedFcn = createCallbackFcn(app, @FindSoundSourceButtonPushed, true);
-            app.FindSoundSourceButton.Position = [50 196 118 23];
+            app.FindSoundSourceButton.Position = [50 139 118 23];
             app.FindSoundSourceButton.Text = 'Find Sound Source';
-    
+
             % Create XCoordinateEditFieldLabel
             app.XCoordinateEditFieldLabel = uilabel(app.UIFigure);
             app.XCoordinateEditFieldLabel.HorizontalAlignment = 'right';
-            app.XCoordinateEditFieldLabel.Position = [6 296 76 22];
+            app.XCoordinateEditFieldLabel.Position = [6 239 76 22];
             app.XCoordinateEditFieldLabel.Text = 'X-Coordinate';
-    
+
             % Create XCoordinateEditField
             app.XCoordinateEditField = uieditfield(app.UIFigure, 'numeric');
-            app.XCoordinateEditField.Position = [97 296 100 22];
-    
+            app.XCoordinateEditField.Position = [97 239 100 22];
+
             % Create YCoordinateEditFieldLabel
             app.YCoordinateEditFieldLabel = uilabel(app.UIFigure);
             app.YCoordinateEditFieldLabel.HorizontalAlignment = 'right';
-            app.YCoordinateEditFieldLabel.Position = [7 256 75 22];
+            app.YCoordinateEditFieldLabel.Position = [7 199 75 22];
             app.YCoordinateEditFieldLabel.Text = 'Y-Coordinate';
-    
+
             % Create YCoordinateEditField
             app.YCoordinateEditField = uieditfield(app.UIFigure, 'numeric');
-            app.YCoordinateEditField.Position = [97 256 100 22];
-    
+            app.YCoordinateEditField.Position = [97 199 100 22];
+
             % Create Label
             app.Label = uilabel(app.UIFigure);
-            app.Label.Position = [130 149 50 22];
+            app.Label.Position = [130 92 50 22];
             app.Label.Text = '';
-    
+
             % Create Label_2
             app.Label_2 = uilabel(app.UIFigure);
-            app.Label_2.Position = [130 115 53 22];
+            app.Label_2.Position = [130 58 53 22];
             app.Label_2.Text = '';
-    
+
             % Create X_PredictedLabel
             app.X_PredictedLabel = uilabel(app.UIFigure);
-            app.X_PredictedLabel.Position = [50 149 70 22];
+            app.X_PredictedLabel.Position = [50 92 70 22];
             app.X_PredictedLabel.Text = 'X_Predicted';
-    
+
             % Create Y_PredictedLabel
             app.Y_PredictedLabel = uilabel(app.UIFigure);
-            app.Y_PredictedLabel.Position = [50 115 70 22];
+            app.Y_PredictedLabel.Position = [50 58 70 22];
             app.Y_PredictedLabel.Text = 'Y_Predicted';
-    
+
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
         end
     end
-    
+
     % App creation and deletion
     methods (Access = public)
-    
+
         % Construct app
-        function app = GUI
-    
+        function app = app1
+
             % Create UIFigure and components
             createComponents(app)
-    
+
             % Register the app with App Designer
             registerApp(app, app.UIFigure)
-    
+
+            % Execute the startup function
+            runStartupFcn(app, @startupFcn)
+
             if nargout == 0
                 clear app
             end
         end
-    
+
         % Code that executes before app deletion
         function delete(app)
-    
+
             % Delete UIFigure when app is deleted
             delete(app.UIFigure)
         end
