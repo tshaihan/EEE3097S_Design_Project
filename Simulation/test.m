5ebb098a1f2f260ec38f4968bcf271fdca7b3b1a
Tilal Mukhtar <mkhtil001@myuct.ac.za>
2023-09-14 20:00:11 +0200

    Update simulation test

--- Simulation/test.m
+++ Simulation/test.m
@@ -37,7 +37,7 @@ function test
     fprintf(fileID, 'x; y; x estimate; y estimate; sync error; TDOA error; position error; latency\n');
     latency = 0.001;
     for i = 1:11
-        [coords, syncError, tdoaError, coordError] = simulation(0.5, 0.25, 65, 48000, 15000, latency);
+        [coords, syncError, tdoaError, coordError] = simulation(0.5, 0.25, 65, 48000, 10000, latency);
         fprintf(fileID, '0.5; 0.25; %f; %f;', coords(1), coords(2));
         posError = sqrt(coordError(1)^2+coordError(2)^2);
         fprintf(fileID, '%f; %f; %f; %f\n', mean(syncError), mean(tdoaError), posError, latency);
@@ -47,22 +47,26 @@ function test
     fprintf(fileID, 'Calibration Signal Position Error Factor\n');
     fprintf(fileID, 'x; y; x estimate; y estimate; sync error; TDOA error; position error; calibration signal position error factor\n');
     calPosError = 0.001;
-    for i = 1:21
-        [coords, syncError, tdoaError, coordError] = simulation(0.5, 0.25, 65, 48000, 15000, 0.01, calPosError);
-        fprintf(fileID, '0.5; 0.25; %f; %f;', coords(1), coords(2));
-        posError = sqrt(coordError(1)^2+coordError(2)^2);
-        fprintf(fileID, '%f; %f; %f; %f\n', mean(syncError), mean(tdoaError), posError, calPosError);
+    for i = 1:20
+        for j = 1:3
+            [coords(j,:), syncError(j,:), tdoaError(j,:), coordError(j,:)] = simulation(0.5, 0.25, 65, 48000, 10000, 0.01, calPosError);
+        end
+        fprintf(fileID, '0.5; 0.25; %f; %f;', mean(coords(:,1)), mean(coords(:,2)));
+        posError = sqrt(mean(coordError(:,1))^2+mean(coordError(:,2))^2);
+        fprintf(fileID, '%f; %f; %f; %f\n', mean(syncError,'all'), mean(tdoaError,'all'), posError, calPosError);
         calPosError = calPosError + 0.001;
     end
 
     fprintf(fileID, 'Mic Position Error Factor\n');
     fprintf(fileID, 'x; y; x estimate; y estimate; sync error; TDOA error; position error; mic position error factor\n');
     micPosError = 0.001;
-    for i = 1:21
-        [coords, syncError, tdoaError, coordError] = simulation(0.5, 0.25, 65, 48000, 15000, 0.01, 0.005, micPosError);
-        fprintf(fileID, '0.5; 0.25; %f; %f;', coords(1), coords(2));
-        posError = sqrt(coordError(1)^2+coordError(2)^2);
-        fprintf(fileID, '%f; %f; %f; %f\n', mean(syncError), mean(tdoaError), posError, micPosError);
+    for i = 1:20
+        for j = 1:3
+            [coords(j,:), syncError(j,:), tdoaError(j,:), coordError(j,:)] = simulation(0.5, 0.25, 65, 48000, 10000, 0.01, 0.001, micPosError);
+        end
+        fprintf(fileID, '0.5; 0.25; %f; %f;', mean(coords(:,1)), mean(coords(:,2)));
+        posError = sqrt(mean(coordError(:,1))^2+mean(coordError(:,2))^2);
+        fprintf(fileID, '%f; %f; %f; %f\n', mean(syncError,'all'), mean(tdoaError,'all'), posError, micPosError);
         micPosError = micPosError + 0.001;
     end
 
@@ -70,7 +74,7 @@ function test
     fprintf(fileID, 'x; y; x estimate; y estimate; sync error; TDOA error; position error; source signal frequency\n');
     srcFreq = 10;
     for i = 1:11
-        [coords, syncError, tdoaError, coordError] = simulation(0.5, 0.25, 65, 48000, 15000, 0.01, 0.005, 0.005, srcFreq);
+        [coords, syncError, tdoaError, coordError] = simulation(0.5, 0.25, 65, 48000, 10000, 0.01, 0.001, 0.001, srcFreq);
         fprintf(fileID, '0.5; 0.25; %f; %f;', coords(1), coords(2));
         posError = sqrt(coordError(1)^2+coordError(2)^2);
         fprintf(fileID, '%f; %f; %f; %d\n', mean(syncError), mean(tdoaError), posError, srcFreq);
